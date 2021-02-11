6f85aba11cdbd528aec17ae36fe6bff930bb1f75
Jonas Reucher <jonasreucher@gmail.com>
2021-02-11 13:52:00 +0100

    Objectification

---
+++ bpsk.hpp
@@ -0,0 +1,30 @@
+#pragma once
+#include <itpp/comm/modulator.h>
+#include <itpp/comm/channel.h>
+#include <itpp/base/math/trig_hyp.h>
+#include <cmath>
+#include <itpp/comm/hammcode.h>
+#include <itpp/stat/misc_stat.h>
+#include "libdsp/Biquad.h"
+
+class BPSK
+{
+private:
+double carrier_freq;
+double sampling_freq;
+double time_per_sample;
+itpp::BPSK* modulator = new itpp::BPSK();
+itpp::Hamming_Code* fec = new itpp::Hamming_Code(2);
+Biquad* bandpass = new Biquad();
+
+public:
+    BPSK();
+    BPSK(double carrier_freq, double sampling_freq);
+    void set_parameters(double &carrier_freq, double &sampling_freq);
+    itpp::vec send(itpp::bvec &signal);
+    itpp::bvec receive(itpp::vec &signal);
+
+    ~BPSK();
+};
+
+
